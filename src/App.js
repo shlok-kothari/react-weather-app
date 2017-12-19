c83f65e47b49896992771a49c18c6bed515cd765
Shlok Kothari <shlokkothari@MacBook-Pro.local>
2017-12-19 11:57:15 +0400

    Added textbox with city search functionality

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import './bootstrap.css';
 import './weather-icons-master/css/weather-icons.css';
+import {DebounceInput} from 'react-debounce-input';
 
 class App extends Component {
   constructor(){
@@ -9,12 +10,13 @@ class App extends Component {
     this.state = {
         weathers: [],
         city: "",
-        country: ""
-
+        country: "",
+        textVal : ""
     };
   }
-  componentDidMount(){
-    fetch('http://api.openweathermap.org/data/2.5/forecast/daily?q=Richardson,us&cnt=6&APPID=71c32854c184fc76cc993c70fb76fa64&units=metric&mode=JSON')
+
+  getWeather(cityName){
+    fetch(`http://api.openweathermap.org/data/2.5/forecast/daily?q=${cityName},us&cnt=6&APPID=71c32854c184fc76cc993c70fb76fa64&units=metric&mode=JSON`)
       .then(response => response.json())
       .then(data => this.setState({ weathers: data.list, city: data.city.name, country: data.city.country}))
       .catch(error => console.log(error) );
@@ -24,23 +26,34 @@ class App extends Component {
       var divStyle = {
         margin: '0px 20px 0px -20px',
         textAlign: 'center',
-        backgroundColor: '#f2f2f2',
+        backgroundColor: '#f9f9f9',
         overflow: 'auto'
       };
       var iconStyle ={
-        fontSize: 50
+        fontSize: 60,
+        color: '#02388e'
       };
       return(  
         <div className="container">
-          <div className="row col-md-8">
-            <h4>{city}, {country}</h4>
+          <br/>
+          <div className="row">
+            <div className="col-md-8">
+              <h4>{city}, {country}</h4>
+            </div>
+            <div className="col-md-4">
+              <DebounceInput minLength={2} className="input" debounceTimeout={300} onChange={event => this.getWeather(event.target.value)} />
+
+            </div>
           </div>
           <div className="row" style={divStyle}>
               {weathers.map(weather =>
                   <div key={weather.dt} className="col-md-2">
-                      <i className={"wi wi-owm-" + weather.weather[0].id +" wi-fw"} style={iconStyle}></i>
-                      <h5>{weather.weather[0].description}</h5>
-                      <h5>{Math.round(weather.temp.max)}° <small className="text-muted">{Math.round(weather.temp.min)}°</small></h5>
+                    <br/>
+                    <i className={"wi wi-owm-" + weather.weather[0].id +" wi-fw"} style={iconStyle}></i>
+                    <br/>
+                    <br/>
+                    <h5>{weather.weather[0].description}</h5>
+                    <h5>{Math.round(weather.temp.max)}° <small className="text-muted">{Math.round(weather.temp.min)}°</small></h5>
                  </div>
              )}
           </div>
