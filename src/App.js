e86b527e83669a8766ce521a8c2228759fc54957
Shlok Kothari <shlokkothari@MacBook-Pro.local>
2017-12-27 13:06:35 +0400

    Added date

--- src/App.js
+++ src/App.js
@@ -35,6 +35,16 @@ class App extends Component {
       .then(data => this.setState({ weathers: data.list, city: data.city.name, country: data.city.country}))
       .catch(error => console.log(error) );
   }
+
+  timeConverter(UNIX_timestamp){
+    var a = new Date(UNIX_timestamp * 1000);
+    var months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+    var year = a.getFullYear();
+    var month = months[a.getMonth()];
+    var date = a.getDate();
+    var time = date + ' ' + month + ' ' + year;
+    return time;
+  }
   render(){
       const {weathers, city, country} = this.state;
       var divStyle = {
@@ -65,11 +75,13 @@ class App extends Component {
           <div className="row" style={divStyle}>
               {weathers.map(weather =>
                   <div key={weather.dt} className="col-md-2">
+                    <h5>{this.timeConverter(weather.dt)}</h5>
                     <br/>
                     <i className={"wi wi-owm-" + weather.weather[0].id +" wi-fw"} style={iconStyle}></i>
                     <br/><br/>
                     <h5>{weather.weather[0].description}</h5>
                     <h5>{Math.round(weather.temp.max)}° <small className="text-muted">{Math.round(weather.temp.min)}°</small></h5>
+                    <h5>Humidity: {weather.humidity}%</h5>
                  </div>
              )}
           </div>
